73e73f21097d889e04d9457d1598ca5a52fc01a6
Rob Winch <rwinch@users.noreply.github.com>
2021-10-26 15:15:32 -0500

    Use content of antora.component.version as version
    Previously it tried to extract values for prerelease, but this can be done
    by publishing the prelease value to the generated repository's antora.yml

--- antora/extensions/dynamic-component-version.js
+++ antora/extensions/dynamic-component-version.js
@@ -4,14 +4,8 @@ module.exports.register = (pipeline, { playbook, config }) => {
     pipeline.on('contentAggregated', ({ contentAggregate }) => {
         contentAggregate.forEach(ca => {
             ca.files.filter(f => f.path === 'antora.component.version').forEach(f => {
-                const fullVersion = f.contents.toString().trim();
-                const separatorIndex = fullVersion.indexOf("-");
-                const version = separatorIndex > 0 ? fullVersion.substr(0, separatorIndex) : fullVersion;
-                const prerelease = separatorIndex > 0 ? fullVersion.substr(separatorIndex) : undefined;
+                const version = f.contents.toString().trim();
                 ca.version = version;
-                if (prerelease) {
-                    ca.prerelease = prerelease;
-                }
             });
         });
     });
