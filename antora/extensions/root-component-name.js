075a1a7a40825c387a445b786b98366ef8d6d519
Rob Winch <rwinch@users.noreply.github.com>
2021-10-27 17:49:43 -0500

    Fix root-component-name to handle aliases

--- antora/extensions/root-component-name.js
+++ antora/extensions/root-component-name.js
@@ -7,12 +7,24 @@ module.exports.register = (pipeline, { config }) => {
     pipeline.on('contentClassified', ({ contentCatalog }) => {
         const rootComponentName = config.rootComponentName || 'ROOT'
         const rootComponentNameLength = rootComponentName.length
-        contentCatalog.findBy({ component: rootComponentName }).filter((file) => file.out).forEach((file) => {
-            file.out.dirname = file.out.dirname.substr(rootComponentNameLength)
-            file.out.path = file.out.path.substr(rootComponentNameLength + 1)
-            file.out.rootPath = file.out.rootPath.split('/').slice(1).join('/') || '.'
-            file.pub.url = file.pub.url.substr(rootComponentNameLength + 1)
-            file.pub.rootPath = file.out.rootPath
+        contentCatalog.findBy({ component: rootComponentName }).forEach((file) => {
+            if (file.out) {
+                file.out.dirname = file.out.dirname.substr(rootComponentNameLength)
+                file.out.path = file.out.path.substr(rootComponentNameLength + 1)
+                file.out.rootPath = fixPath(file.out.rootPath)
+            }
+            if (file.pub) {
+                file.pub.url = file.pub.url.substr(rootComponentNameLength + 1)
+                if (file.pub.rootPath) {
+                    file.pub.rootPath = fixPath(file.pub.rootPath)
+                }
+            }
+            if (file.rel) {
+                if (file.rel.pub) {
+                    file.rel.pub.url = file.rel.pub.url.substr(rootComponentNameLength + 1) || "/"
+                    file.rel.pub.rootPath = fixPath(file.rel.pub.rootPath);
+                }
+            }
         })
         const rootComponent = contentCatalog.getComponent(rootComponentName)
         rootComponent?.versions?.forEach((version) => {
@@ -21,4 +33,8 @@ module.exports.register = (pipeline, { config }) => {
         // const siteStartPage = contentCatalog.getById({ component: '', version: '', module: '', family: 'alias', relative: 'index.adoc' })
         // if (siteStartPage) delete siteStartPage.out
     })
+
+    function fixPath(path) {
+        return path.split('/').slice(1).join('/') || '.'
+    }
 }
